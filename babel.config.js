8aaefb35871bf59127d65482a1a85c561a4c33e5
salvatoreDeploy <liderhenrique@gmail.com>
2022-01-27 20:31:18 -0300

    Atualizaçao 27/01/2022

--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,11 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@error": ["./src/error"],
-          "@utils": ["./src/utils"],
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@error": "./src/error",
+          "@utils": "./src/utils",
         },
       },
     ],
