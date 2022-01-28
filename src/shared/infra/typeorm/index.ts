4fd4340ba8834bc1f92f51ea236c3cc73dd40345
salvatoreDeploy <liderhenrique@gmail.com>
2022-01-27 21:37:24 -0300

    database Docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -12,12 +12,25 @@ getConnectionOptions().then((options) => {
     });
 }); */
 
-export default async (host = "localhost"): Promise<Connection> => {
+/* export default async (host = "localhost"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentalx_test"
+          : defaultOptions.database,
+    })
+  );
+}; */
+
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
       database:
         process.env.NODE_ENV === "test"
           ? "rentalx_test"
