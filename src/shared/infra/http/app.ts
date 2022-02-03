6b58b38d35fe042fa5faafbed6116ba4393f710e
salvatoreDeploy <liderhenrique@gmail.com>
2022-02-02 23:05:36 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,6 +11,7 @@ import createConnection from "@shared/infra/typeorm";
 import swaggerFile from "../../../swagger.json";
 import { routes } from "./routes";
 import upload from "@config/upload";
+import cors from "cors";
 
 createConnection();
 
@@ -22,6 +23,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 
 app.use(routes);
 
