e4214ffbf356b7e35387cb21b2558fd9199112a2
salvatoreDeploy <liderhenrique@gmail.com>
2022-02-03 22:23:51 -0300

    Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,8 @@ import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { routes } from "./routes";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import rateLimiter from "./middlewares/rateLimiter";
 import upload from "@config/upload";
 import cors from "cors";
@@ -20,7 +22,22 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
+
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -30,6 +47,8 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
