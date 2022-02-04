9988af8d6c098b27bf08dcb4f4e687137bd76693
salvatoreDeploy <liderhenrique@gmail.com>
2022-02-03 23:00:57 -0300

    Finish Production

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,23 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-/* interface IOptions {
-    host: string;
-}
-
-getConnectionOptions().then((options) => {
-    const newOptions = options as IOptions;
-    newOptions.host = "database";
-    createConnection({
-        ...options,
-    });
-}); */
-
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentalx_test"
@@ -25,16 +12,3 @@ export default async (host = "localhost"): Promise<Connection> => {
     })
   );
 };
-
-/* export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
-
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === "test"
-          ? "rentalx_test"
-          : defaultOptions.database,
-    })
-  );
-}; */
